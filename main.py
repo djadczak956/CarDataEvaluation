ff2a7d738b2c7666cd7739b1e48d931768d0a4bf
Damian Jadczak <62266703+djadczak956@users.noreply.github.com>
2021-12-16 12:39:18 -0500

    Uploading files
    Ye

---
+++ main.py
@@ -0,0 +1,51 @@
+# Import stuff
+import numpy as np
+import pandas as pd
+from sklearn import neighbors, metrics
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+
+data = pd.read_csv('car.data')
+#print(data.head())
+
+X = data[[      # The more important features of data
+    'buying',
+    'maint',
+    'safety'
+]].values
+y = data[['class']]
+#print(X, y)
+
+# Converting the X data
+Le = LabelEncoder()
+for i in range(len(X[0])):  # The length of X[0] is 3 in this case
+    X[:, i] = Le.fit_transform(X[:, i]) # Looping through the index by i
+
+#print(X)
+
+# Converting the Y data
+label_mapping = {
+    'unacc':0,
+    'acc':1, 
+    'good':2,
+    'vgood':3
+}
+y['class'] = y['class'].map(label_mapping)
+y = np.array(y)
+print(y)
+
+# Creating model
+knn = neighbors.KNeighborsClassifier(n_neighbors=25, weights='uniform')
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2) # Split data into training and testing
+
+knn.fit(X_train, y_train)   # Fit the training data
+prediction = knn.predict(X_test)   # Predict with the testing data of X
+
+accuracy = metrics.accuracy_score(y_test, prediction)   
+
+print("Predictions --> ", prediction)
+print("Accuracy", accuracy)
+
+print("Actual value -->", y[20])
+print("Predicted value --> ", knn.predict(X)[20])
